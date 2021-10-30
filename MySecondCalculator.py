546835422be1b85542686c1ee1304bc448d64c30
Andrey Gusarov <AGusarov@live.ag>
2021-10-30 15:40:00 +0300

    calc

--- MySecondCalculator.py
+++ MySecondCalculator.py
@@ -1,10 +1,14 @@
-splus = "yes" # создаём переменную со значением да 
-sminus = "no" # создаём переменную со значением нет
-result = input("Дневник прислал вовремя? (да/нет)") #создаём присвоения значения
-result2 = input() #создаём присвоение значения
-result3 = input() #создаём присвоения значения
-if splus == "y":
-    print ("+10 min")
-elif sminus == "n":
-    print ("-10 min")
-print(int(result3 + (result2)
+DaysOfWeek = ["Пн", "Вт", "Ср", "Чт", "Пт"]
+
+playtime = int (input("Сколько времени у тебя в начала недели? "))
+MIN_TIME = 1
+MAX_TIME = 60
+
+for day in DaysOfWeek:
+    result = input(day + " - Дневник прислал вовремя? (y/n)") #создаём присвоения значения
+    if result == "y":
+        playtime = min(playtime + 10, MAX_TIME)
+    elif result == "n":
+        playtime = max(playtime - 10, MIN_TIME)
+
+    print(playtime)
